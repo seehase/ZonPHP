cfe83b21a26f70d7800d2fa8484a2b7d213e9ed2
ericplan <48489161+ericplan@users.noreply.github.com>
2019-04-29 12:14:37 +0200

    Add files via upload
    New version to repair bug with reloading when navigating, use only in combination with new day_chart.php. Make full use of Highcharts. Removed inverter dropdown and other stuff. Moved $chartdate to this file, causing minor incompatibility with index.php

--- ZonPHP/day_overview.php
+++ ZonPHP/day_overview.php
@@ -5,44 +5,25 @@ include_once "inc/sessionstart.php";
 include_once "inc/load_cache.php";
 include_once "inc/import_data.php";
 
+$chartdate=@mktime();
+
+if(isset($_GET['dag']))
+	$_SESSION['sesdag']=strtotime($_GET['dag']); 
+
+if(isset($_SESSION['sesdag'])){	
+	if(isset($_GET['+']))	
+		$_SESSION['sesdag']=strtotime("+1 day", $_SESSION['sesdag']);
+	if(isset($_GET['-']))
+		$_SESSION['sesdag']=strtotime("-1 day", $_SESSION['sesdag']);
+	if(isset($_GET['Vandaag']))
+		$_SESSION['sesdag'] = $chartdate;
+}
+else
+	$_SESSION['sesdag'] = $chartdate;
+?>
+<?php
 include_once "inc/header.php";
 include_once "charts/day_chart.php";
-
-
-$inverter = $_SESSION['Wie'];
-if (isset($_POST['inverter'])) {
-    $inverter = $_POST['inverter'];
-}
-
-if (isset($_GET['naam'])) {
-    $inverter = $_GET['naam'];
-}
-
-$inverter_id = $inverter;
-$add_params = "";
-if ((isset($_POST['type']) && ($_POST['type'] == "all")) ||
-    (isset($_GET['type']) && ($_GET['type'] == "all"))) {
-    $inverter_id = "all";
-    $add_params = "&type=all";
-}
-
-
-$nextdatevisible = false;
-$nextdate = strtotime("+1 day", $chartdate);
-$nextdatestring = strftime("%Y-%m-%d", $nextdate);
-if ($nextdate <= $date_maximum) {
-    $nextdatevisible = true;
-}
-
-$prevdatevisible = false;
-$prevdate = strtotime("-1 day", $chartdate);
-$prevdatestring = strftime("%Y-%m-%d", $prevdate);
-if ($prevdate >= $date_minimum) {
-    $prevdatevisible = true;
-}
-$chartdaydatestring = strftime("%Y-%m-%d", strtotime("+0 day", $date_maximum));
-
-
 ?>
 
 <?php include_once "menu.php"; ?>
@@ -75,91 +56,34 @@ $chartdaydatestring = strftime("%Y-%m-%d", strtotime("+0 day", $date_maximum));
     });
 </script>
 
-
-<?php
-    $choose_inverter_dropdown = "";
-    $multiple_inverters = false;
-    $choose_inverter_items = "";
-    $paramstr_choose = '';
-    $paramstr_day = '';
-    # remove naam parameter
-    if (sizeof($_GET) > 0){
-        foreach ($_GET as $key => $value) {
-            if ( !(($key == "naam") || ($key == "type")) ) {
-                $paramstr_choose .=  $key . "=" . $value . "&";
-            }
-            if ( $key != "dag") {
-                $paramstr_day .= $key . "=" . $value . "&";
-            }
-        }
-    }
-    if (strpos($paramstr_day, "?") == 0) {
-        $paramstr_day = '?' . $paramstr_day;
-    }
-    if (strpos($paramstr_choose, "?") == 0) {
-        $paramstr_choose = '?' . $paramstr_choose;
-    }
-    foreach ($sNaamSaveDatabase as $key => $sdbnaam) {
-        $choose_inverter_items .= "<li><a href='" . $_SERVER['SCRIPT_NAME'] . $paramstr_choose . "naam=" . $sdbnaam .
-            "' onclick=\"target='_self'\">" . $sdbnaam . "</a></li>";
-    }
-
-    if (strlen($choose_inverter_items) > 0){
-        $choose_inverter_dropdown = '
-                    <div style="position: absolute; z-index: 50">
-    
-                        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" style="margin-top: 15px;margin-left: 20px;">' .
-                            $txt['choose_inverter'] . '
-                            <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu"> ' .
-                            $choose_inverter_items .
-                            "<li><a href='" . $_SERVER['SCRIPT_NAME'] . $paramstr_choose . "type=all" .
-                            "' onclick=\"target='_self'\">" . $txt['all_inverters'] . "</a></li>" . '
-                        </ul>
-                    </div>
-            
-            ';
-        $multiple_inverters = true;
-    }
-?>
-
-
 <div id="page-content">
 
     <div id='resize' class="bigCharts" style="<?= WINDOW_STYLE_CHART ?>; padding-bottom: 72px; ">
 
         <div id="week_chart_header" class="<?= HEADER_CLASS ?>">
-
-            <?php
-                if ($multiple_inverters) echo $choose_inverter_dropdown;
-            ?>
-
-            <h2>
-                <?php
-                if ($prevdatevisible) {
-                    echo '<a class="btn btn-primary" href="day_overview.php' . $paramstr_day .'dag=' .  $prevdatestring .  '"> < </a>';
-                }
-                echo " " . $datum . " " . '  <input type="hidden" id="startdate" value="' . strftime("%d-%m-%Y", time()) . '" readonly>  ';
-                if ($nextdatevisible) {
-                    echo '<a class="btn btn-primary" href="day_overview.php' . $paramstr_day .'dag=' .  $nextdatestring . '"> > </a>';
-                }
-                ?>
-            </h2>
-
+            <form action = "<?php $_SERVER['PHP_SELF']; ?>" method = "GET">
+				<h2 align="center" class="notopgap" >	
+					<?php if($date_minimum<$_SESSION['sesdag'])
+						echo '<INPUT class="btn btn-primary" type="submit" name="-" value="<">';
+						echo " ".$datum." ";
+					 	if($date_maximum>$_SESSION['sesdag'])
+							echo '<INPUT class="btn btn-primary" type="submit" name="+" value=">">';
+					?>	
+				</h2>		
+			</form>			
         </div>
 
 
-
-        <div id="mycontainer_<?php echo $inverter_id ?>" style="width:100%; height:100%;"></div>
+		<div id="mycontainer" style="width:100%; height:100%;"></div>
 
     </div>
 
     <div style="float: unset; margin-top: 5px;">
         <button class="btn btn-primary" id="toggelbutton"><?php echo $txt['showvalues'] ?></button>
-        <a  href="<?php echo "day_overview.php".$paramstr_day."dag=".$chartdaydatestring ?>" target="_self">
-            <button class="btn btn-primary"><?php echo $txt['back_to_today'] ?> </button>
-        </a>
+        <form action = "<?php $_SERVER['PHP_SELF']; ?>" method = "GET">
+		<INPUT class="btn btn-primary" type="submit" name="Vandaag"  value="<?php echo $txt["terugnaarvandaag"]; ?>"><br>
+	</form>
+        
     </div>
 
     <div id="kalender">
