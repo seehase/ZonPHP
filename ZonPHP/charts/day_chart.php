0900fa03dfff7c367d7e2a8885cfe246f5360017
Holger Seehausen <seehausen@gmx.de>
2019-05-03 14:02:01 +0200

    Fixed Temperature line

--- ZonPHP/charts/day_chart.php
+++ ZonPHP/charts/day_chart.php
@@ -172,96 +172,6 @@ if (mysqli_num_rows($resultmd) == 0) {
 //--------------------------------------------------------------------------------------------------
 
 
-// ---SENSOR -----------------------------------------------------------------------------------------------------------
-$sensor_values = array();
-// init array for the hole day
-for ($i = 0; $i < 24; $i++) {
-    for ($j = 0; $j < 12; $j++) {
-        $sensor_values[date("H:i", strtotime($i . ":" . $j * 5))] = "";
-    }
-}
-$sensorid = 197190;
-$sensortype = 1;
-
-$temp_vals = array();
-
-$external_sensors = isset($param['external_sensors']);
-
-if ($external_sensors) {
-    $tablename = $table_prefix . "_sensordata";
-    $result = mysqli_query($con, "SHOW TABLES LIKE '" . $tablename . "'");
-    if ($result->num_rows == 0) {
-        $external_sensors = false;
-    }
-}
-$sensor_available = ($external_sensors == true) || ($use_weewx == true);
-
-$val_min = 500;
-$val_max = -500;
-
-if (!isset($weewx_table_name)) $weewx_table_name = "archive";
-if (!isset($weewx_temp_column)) $weewx_temp_column = "outTemp";
-if (!isset($weewx_timestamp_column)) $weewx_timestamp_column = "dateTime";
-if (!isset($weewx_temp_is_farenheit)) $weewx_temp_is_farenheit = true;
-
-$sensor_success = false;
-$temp_unit = "°C";
-
-if ($sensor_available) {
-    $val_avg = 0;
-    if (isset($param['external_sensors_for_daychart'])) {
-        // use external arexx sensors
-        $sql_sensor =
-            "  SELECT 
-                   AVG( measurevalue ) AS val,
-                   STR_TO_DATE( CONCAT( DATE( logtime ) ,  ' ',HOUR( logtime ) , ':', LPAD( FLOOR( MINUTE( logtime ) /5 ) *5, 2, '0' ) , ':00' ) ,
-                       '%Y-%m-%d %H:%i:%s' ) AS nicedate 
-                FROM $tablename 
-                WHERE logtime  LIKE '" . $chartdatestring . "%' AND sensorid= $sensorid AND sensortype = $sensortype
-                GROUP BY nicedate ORDER BY nicedate ASC";
-        $result_sensor = mysqli_query($con, $sql_sensor) or die("Query failed. dag " . mysqli_error($con));
-        $sensor_success = true;
-    } else if ($use_weewx == true) {
-        // use weewx connection and table
-        $sql_sensor =
-            "   SELECT 
-                   AVG( $weewx_temp_column ) AS val,
-                   STR_TO_DATE( CONCAT( DATE( from_unixtime($weewx_timestamp_column )) ,  ' ' ,HOUR( from_unixtime($weewx_timestamp_column) ) , ':', 
-                   LPAD( FLOOR( MINUTE( from_unixtime($weewx_timestamp_column) ) /5 ) *5, 2, '0' ) , ':00' ) ,
-                       '%Y-%m-%d %H:%i:%s' ) AS nicedate 
-                FROM $weewx_table_name 
-                WHERE from_unixtime($weewx_timestamp_column)  LIKE '" . $chartdatestring . "%'
-                GROUP BY nicedate ORDER BY nicedate ASC";
-        $result_sensor = mysqli_query($con_weewx, $sql_sensor) or die("Query failed. dag " . mysqli_error($con));
-        $sensor_success = true;
-    }
-
-    if ($sensor_success == true && mysqli_num_rows($result_sensor) != 0) {
-        while ($row = mysqli_fetch_array($result_sensor)) {
-            // array time = value
-            if ($weewx_temp_is_farenheit) {
-                $val = number_format(($row['val'] - 32) * 5 / 9, 1); // F --> °C
-                $temp_unit = "°C";
-            } else {
-                $val = number_format($row['val'], 1); // temp is already in °C
-                $temp_unit = "°F";
-            }
-            $sensor_values[date("H:i", strtotime($row['nicedate']))] = $val;
-            $temp_vals[strtotime($row['nicedate'])] = $val;
-            if ($val > $val_max) $val_max = $val;
-            if ($val < $val_min) $val_min = $val;
-        }
-
-        // enlarge y-axis if needed
-        $val_dif = abs($val_max - $val_min);
-        if ($val_dif < 5) {
-            $val_min = $val_min - 3;
-            $val_max = $val_max + 3;
-        };
-    }
-
-// ---SENSOR -----------------------------------------------------------------------------------------------------------
-}
 
 
 $strgegmax = "";
@@ -344,20 +254,13 @@ $max_last_val = $newDate;
 $str_max = substr($str_max, 0, -1);
 $strgegmax = substr($strgegmax, 0, -1);
 
-// temp line --------------------------------------------------------------
-$str_temp_vals = "";
-if ($sensor_success) {
-    $str_temp_vals = "";
-    foreach ($temp_vals as $time => $val) {
-        if (($time > $max_first_val) && ($time < $max_last_val)) {
-            if (isset($param['no_units'])) {
-                $str_temp_vals .= "[" . $time * 1000 . "," . number_format($val, 1, '.', '') . "],";
-            } else {
-                $str_temp_vals .= "{x:" . $time * 1000 . ", y:" . number_format($val, 1, '.', '') . ", unit: '" . $temp_unit . "' },";
-            }
-        }
-    }
-    $str_temp_vals = substr($str_temp_vals, 0, -1);
+
+
+$external_sensors = isset($param['external_sensors']);
+$temp_serie = "";
+$temp_unit = "°C";
+if ($external_sensors) {
+    include "charts/temp_sensor_inc.php";
 }
 
 // cumulative line --------------------------------------------------------------
@@ -378,6 +281,7 @@ foreach ($aoplopendkwdag as $tuur => $fkw) {
     $cum_max_value = $fkw;
 }
 $str_cum = substr($str_cum, 0, -1);
+
 if (strlen($str_dataserie) == 0) $str_cum = "";
 
 $strsomkw = substr($strsomkw, 0, -1);
@@ -427,7 +331,8 @@ include_once "chart_styles.php";
 
 $show_temp_axis = "false";
 $show_cum_axis = "true";
-if (strlen($str_temp_vals) > 0) {
+
+if (strlen($temp_serie) > 0) {
     $show_temp_axis = "true";
     $show_cum_axis = "false";
 }
@@ -442,10 +347,13 @@ if (strlen($str_temp_vals) > 0) {
         var khhWp = [<?php echo $param['ieffectief_kwpiekst'] ?>];
         var maxmax = <?php echo json_encode($maxkwh) ?>;
         var maxlink = '<?php echo $maxlink ?>';
+        var temp_max = <?php echo $val_max ?>;
+        var temp_min = <?php echo $val_min ?>;
         var txt_actueel = '<?php echo $txt['actueel'] ?>';
         var txt_totaal = '<?php echo $txt['totaal'] ?>';
         var txt_max = '<?php echo $txt['max'] ?>';
         var txt_peak = '<?php echo $txt['peak'] ?>';
+
 
         Highcharts.setOptions({<?php echo $chart_lang ?>});
 
@@ -516,7 +424,7 @@ if (strlen($str_temp_vals) > 0) {
 
                             //function to check amount of visible series and to destroy old spline series
                             mychart.series.forEach(s => {
-                                if (s.type === 'spline' && s.visible === true) {
+                                if (s.type === 'spline' && s.visible === true && s.name != 'Temp')  {
                                     s.destroy()
                                 } else if (s.type === 'spline' && s.visible === false) {
                                     checkHideForSpline = 0
@@ -617,7 +525,7 @@ if (strlen($str_temp_vals) > 0) {
                     }
                 },
                 gridLineColor: '#<?php echo $colors['color_chart_gridline_yaxis1'] ?>'
-            },
+                },
                 { // cum kWh
                     title: {
                         text: 'Total',
@@ -637,10 +545,38 @@ if (strlen($str_temp_vals) > 0) {
                     gridLineColor: '#<?php echo $colors['color_chart_gridline_yaxis2'] ?>',
                     opposite: true,
                     visible: <?php echo $show_cum_axis ?>
+                },
+                { // temperature
+                    title: {
+                        text: 'Temperature',
+                        style: {
+                            color: '#<?php echo $colors['color_chart_title_yaxis3'] ?>',
+                        },
+                    },
+                    labels: {
+                        format: '{value}<?php echo $temp_unit ?>',
+                        style: {
+                            color: '#<?php echo $colors['color_chart_labels_yaxis1'] ?>',
+                        },
+                        formatter: function () {
+                            return this.value + "<?php echo $temp_unit ?>";
+                        },
+                    },
+                    gridLineColor: '#<?php echo $colors['color_chart_gridline_yaxis3'] ?>',
+                    opposite: true,
+                    visible: <?php echo $show_temp_axis ?>,
+                    steps: 5,
+                    min: temp_min,
+                    max: temp_max,
                 }
+
             ],
 
-            series: [ <?php echo $str_dataserie ?> <?php echo $str_max ?> ]
+            series: [
+                <?php echo $str_dataserie ?>
+                <?php echo $str_max ?>
+                <?php echo $temp_serie ?>
+            ]
 
         }), function (mychart) {
             mychart.forRender = true
