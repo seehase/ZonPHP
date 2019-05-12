80ec385f38c6f6836787094ce42c40eb65415a8a
Holger Seehausen <seehausen@gmx.de>
2019-05-12 14:43:56 +0200

    removed Choose Inverter and moved back to today on top

--- ZonPHP/month_overview.php
+++ ZonPHP/month_overview.php
@@ -30,9 +30,6 @@ $chartmonthdatestring = strftime("%Y-%m-01", strtotime("+0 month", $date_maximum
 
 
 <?php
-$choose_inverter_dropdown = "";
-$multiple_inverters = false;
-$choose_inverter_items = "";
 $paramstr_choose = '';
 $paramstr_day = '';
 # remove naam parameter
@@ -52,27 +49,7 @@ if (strpos($paramstr_day, "?") == 0) {
 if (strpos($paramstr_choose, "?") == 0) {
     $paramstr_choose = '?' . $paramstr_choose;
 }
-foreach ($sNaamSaveDatabase as $key => $sdbnaam) {
-    $choose_inverter_items .= '<li><a href="#" onclick="myDropdownFunction(\'' . $sdbnaam . '\')">' . $sdbnaam . '</a></li>';
-}
 
-if (strlen($choose_inverter_items) > 0){
-    $choose_inverter_dropdown = '
-                        <div style="position: absolute; z-index: 50">
-        
-                            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" style="margin-top: 15px;margin-left: 20px;">' .
-        $txt['choose_inverter'] . '
-                                <span class="caret"></span>
-                            </button>
-                            <ul class="dropdown-menu"> ' .
-        $choose_inverter_items .
-        '<li><a href="#" onclick="myDropdownFunction(\'all\')">' . $txt['all_inverters'] . '</a></li>' . '
-                            </ul>
-                        </div>
-                
-                ';
-    $multiple_inverters = true;
-}
 ?>
 
 
@@ -81,9 +58,12 @@ if (strlen($choose_inverter_items) > 0){
 
     <div id='resize' class="bigCharts" style="<?= WINDOW_STYLE_CHART ?>; padding-bottom: 66px; ">
         <div id="week_chart_header" class="<?= HEADER_CLASS ?>">
-            <?php
-            if ($multiple_inverters) echo $choose_inverter_dropdown;
-            ?>
+            <div style="position: absolute; z-index: 50">
+                <form action="<?php $_SERVER['PHP_SELF']; ?>" method="GET">
+                    <button class="btn btn-primary" type="submit" id="txt" name="dag" style="margin-top: 15px;"
+                            value="<?php echo date('Y-m-d', $chartdate); ?>"><?php echo $txt["terugnaarvandaag"] ?></button>
+                </form>
+            </div>
             <h2 align="center">
                 <form action = "<?php $_SERVER['PHP_SELF']; ?>" method = "GET">
                 <?php if(date('Y-m',$date_minimum)<date('Y-m',$chartdate))
@@ -102,10 +82,7 @@ if (strlen($choose_inverter_items) > 0){
 
     <div style="float: unset; margin-top: 5px;">
         <button class="btn btn-primary" id="toggelbutton"><?php echo $txt['showvalues'] ?></button>
-        <form action="<?php $_SERVER['PHP_SELF']; ?>" method="GET">
-            <button class="btn btn-primary" type="submit" id="txt" name="dag"
-                    value="<?php echo date('Y-m-d', $chartdate); ?>"><?php echo $txt["terugnaarvandaag"] ?></button>
-        </form> 
+
     </div>
 
     <div id="tabelgeg">
