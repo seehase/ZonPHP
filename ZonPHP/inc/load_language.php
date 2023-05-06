9093ec9885c6208f2b0ff7a2dad048c5ba3a60ca
ericplan <48489161+ericplan@users.noreply.github.com>
2023-05-06 16:42:40 +0200

    Update load_language.php
    No need to have $formatter in the for loop

--- ZonPHP/inc/load_language.php
+++ ZonPHP/inc/load_language.php
@@ -70,11 +70,12 @@ if ($taal == "en") {
     $locale='en_US'; // For IntlDateFormatter
 }
 // preparing a localized month array
+$formatter = new IntlDateFormatter($locale , IntlDateFormatter::NONE,
+              IntlDateFormatter::NONE, NULL, NULL, "MMMM");
 $months[] = "";
 for ($i = 1; $i <= 12; $i++) {
     if ($use_utf8 == true) {
-        $formatter = new IntlDateFormatter($locale , IntlDateFormatter::NONE,
-              IntlDateFormatter::NONE, NULL, NULL, "MMMM");
+        
 		$months[] = substr(datefmt_format( $formatter, mktime(0, 0, 0, $i)),0,3);
      } else {
         $months[] = utf8_encode(date("M", strtotime("2009-" . $i . "-01")));
