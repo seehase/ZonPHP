8b9d1295b4492a25a11ec1d849daea6d583a5ea3
ericplan <48489161+ericplan@users.noreply.github.com>
2023-05-16 22:00:48 +0200

    Update menu.php
    Removed Fly-out stuff
    Menu is selectable in par_edit and unvisible in header when not selected.
    Minor changes to the flag part.

--- ZonPHP/menu.php
+++ ZonPHP/menu.php
@@ -6,7 +6,6 @@
             navarray[i].checked = false
         }
     }
-
     $('#setQuickVar1').on('click', function() {
         var checkStatus = this.checked ? 'ON' : 'OFF';
 
@@ -15,41 +14,32 @@
                 $('#resultQuickVar1').html(data);
             });
     });
-
 </script>
-
 <?php
-if (!isset($param['flyout'])) {
-    $param['flyout'] = "0";
+$show_menu = "display: none;";
+if (isset($param['show_menu']))
+{
+    $show_menu = "clear:both; ";
 }
-
-if (intval($param['flyout']) > 0) {
-    echo "<div>";
-}
-
-
 ?>
 <input type="checkbox" name="nav" id="main-nav-check"/>
-<div id="menu" style="clear:both; ">
+<div id="menu" style="<?php echo $show_menu; ?>">
     <label for="main-nav-check" class="toggle" onclick="" title="Close"></label>
     <ul>
         <li><a href="#" style="display: flex">&nbsp;</a> <label for="fof" class="toggle-sub" onclick="">
                 &nbsp;<?php echo $txt["grafiekoverzicht"]; ?>&nbsp;&nbsp;&nbsp;&#9658;</label>
             <input type="checkbox" name="nav" id="fof" class="sub-nav-check"/>
             <ul id="fof-sub" class="sub-nav">
-                <li class="sub-heading"><?php echo $txt["grafiekoverzicht"]; ?><label for="fof" class="toggle"
-                                                                                      onclick="" title="Back">
-                        &#9658;</label></li>
-
+                <li class="sub-heading"><?php echo $txt["grafiekoverzicht"]; ?><label for="fof" class="toggle" onclick="" title="Back">&#9658;</label></li>
                 <li><a href="day_overview.php"><?php echo $txt["chart_dayoverview"]; ?></a></li>
-                <li><a href="month_overview.php"><?php echo $txt["chart_monthoverview"]; ?></a></li>
-                <li><a href="year_overview.php"><?php echo $txt["chart_yearoverview"]; ?></a></li>
+                <li><a href="month_overview.php"><?php echo $txt['chart_monthoverview']; ?></a></li>
+                <li><a href="year_overview.php"><?php echo $txt['chart_yearoverview']; ?></a></li>
                 <li><a href="all_years_overview.php"><?php echo $txt["chart_allyearoverview"]; ?></a></li>
                 <li><a href="last_years_overview.php"><?php echo $txt["chart_lastyearoverview"]; ?></a></li>
-                <li><a href="top31.php?Max_Min=Top"><?php echo $txt["chart_31days"]; ?></a></li>
+                <li><a href="cumulative_overview.php"><?php echo $txt["chart_cumulativeoverview"];?></a></li>
+                <li><a href="top31.php"><?php echo $txt["chart_31days"]; ?></a></li>
             </ul>
         </li>
-
         <?php if ($use_weewx == true)
             echo '
             <li><a href="#" style="display: flex">&nbsp;</a> <label for="weewx" class="toggle-sub" onclick="">&nbsp;weewx&nbsp;&nbsp;&#9658;</label>
@@ -57,12 +47,11 @@ if (intval($param['flyout']) > 0) {
             <ul id="weewx-sub" class="sub-nav">
                 <li class="sub-heading">weewx<label for="weewx" class="toggle" onclick="" title="Back">&#9658;</label>
                 </li>
-                <li><a href="weewx_overview.php">weewx-overview</a></li>
-                <li><a href="/weewx/index.html">weewx-website</a></li>                
+                <li><a href="weewx_overview.php">' . $txt["weewx"] . '-overview</a></li>
+                <li><a href="/weewx/index.html">' . $txt["weewx"] . '-website</a></li>                
             </ul>
         </li> ';
         ?>
-
         <li><a href="#" style="display: flex">&nbsp;</a> <label for="themes" class="toggle-sub" onclick="">&nbsp;Themes&nbsp;&nbsp;&nbsp;&#9658;</label>
             <input type="checkbox" name="nav" id="themes" class="sub-nav-check"/>
             <ul id="themes-sub" class="sub-nav">
@@ -76,65 +65,36 @@ if (intval($param['flyout']) > 0) {
                 <li><a href='?theme=theme4'>blue</a></li>
             </ul>
         </li>
-
         <li><a href="#" style="display: flex">&nbsp;</a> <label for="info" class="toggle-sub" onclick=""><span style="text-align: right">&nbsp;Info&nbsp;&nbsp;&nbsp;&#9658;</span></label>
             <input type="checkbox" name="nav" id="info" class="sub-nav-check"/>
             <ul id="info-sub" class="sub-nav">
-                <li class="sub-heading"><?= $version ?><label for="info" class="toggle" onclick="" title="Back">
-                        &#9658;</label>
-                </li>
+                <li class="sub-heading"><?= $version ?><label for="info" class="toggle" onclick="" title="Back">&#9658;</label></li>
                 <li><a href="install/par_welcome.php"><?php echo $txt["login"]; ?> </a></li>
                 <li><a href="about.php">About</a></li>
                 <li><a href="installation.php"><?php echo $txt["installatie"]; ?></a></li>
                 <li><a href="https://github.com/seehase/ZonPHP/">sourcecode</a></li>
                 <li><a href="https://github.com/seehase/ZonPHP/archive/master.zip">download ZonPHP</a></li>
                 <li><a href="inc/destroy.php"><?php echo $txt["clearsession"]; ?> </a></li>
-
             </ul>
         </li>
-
     </ul>
     <label class="toggle close-all" onclick="uncheckboxes('nav')">&times;</label>
-
 </div> <!-- closing "#menu" -->
-
-<?php
-if (intval($param['flyout']) === 2) {
-    echo "</div>";
-}
-?>
-
 <div class="container">
     <div id="header">
-        
         <input   type="image" src="inc/image/logo.png"   onClick="location.href='index.php'" style="position:absolute; top:21px; left:136px;  border:0" value='Vandaag'>
-        
-        <label for="main-nav-check" class="toggle" onclick="" title="Menu">&#x2261;</label>
-        
-        
-        <span style="margin-left: 330px;">&nbsp;</span>
-        <?php if (isset($param['lang_nl'])) echo "<a href='?taal=nl' onclick=\"target='_self'\"><img src='inc/image/nl.svg' border='0' width='16' height='12' alt='Nederlands' title='Nederlands'/></a>"; ?>
-        <?php if (isset($param['lang_en'])) echo "<a href='?taal=en' onclick=\"target='_self'\"><img src='inc/image/en.svg' border='0' width='16' height='12' alt='english' title='english'/></a>" ?>
-        <?php if (isset($param['lang_fr'])) echo "<a href='?taal=fr' onclick=\"target='_self'\"><img src='inc/image/fr.svg' border='0' width='16' height='12' alt='française' title='français'/></a>" ?>
-        <?php if (isset($param['lang_de'])) echo "<a href='?taal=de' onclick=\"target='_self'\"><img src='inc/image/de.svg' border='0' width='16' height='12' alt='deutsch' title='deutsch'/></a>" ?>
-
+ 		<?php if (isset($param['show_menu'])) echo '<label for="main-nav-check" class="toggle" onclick="" title="Menu">&#x2261;</label>'; ?>
+         <span style="margin-left: 330px;">&nbsp;</span>
+        <?php if (isset($param['lang_nl'])) echo "<a href='?taal=nl' onclick=\"target='_self'\"><img class='flag flag-nl' alt='Nederlands' title='Nederlands'/></a>"; ?>
+        <?php if (isset($param['lang_en'])) echo "<a href='?taal=en' onclick=\"target='_self'\"><img class='flag flag-gb' alt='english' title='english'/></a>" ?>
+        <?php if (isset($param['lang_fr'])) echo "<a href='?taal=fr' onclick=\"target='_self'\"><img class='flag flag-fr' alt='français' title='français'/></a>" ?>
+        <?php if (isset($param['lang_de'])) echo "<a href='?taal=de' onclick=\"target='_self'\"><img src='inc/image/blank.gif' class='flag flag-de' alt='deutsch' title='deutsch'/></a>" ?>
         <?php
-
         if ($iveromvormers == 1) {
             echo '<p id="headerinverter" style="margin: -22px 460px 10px">' .
                 $param['sNaamVoorOpWebsite'] .
                 '</p>';
         }
-
         $ligado = 0;
-
         ?>
-
     </div><!-- closing "#header" -->
-
-<?php
-if (intval($param['flyout']) == 1) {
-    echo "</div>";
-}
-?>
-
