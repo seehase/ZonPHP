31bb1b148c0e0efaa1f6b90fa8419824e8a484bf
ericplan <48489161+ericplan@users.noreply.github.com>
2023-05-17 14:06:07 +0200

    Update par_menu.php
    Cleaned up some code, removed Euro entry
    Made language flags dependant on login status

--- ZonPHP/install/par_menu.php
+++ ZonPHP/install/par_menu.php
@@ -8,12 +8,10 @@
                 <?php
                 if (isset($_SESSION['passok']) && $_SESSION['passok'] == "passinorder") {
                     echo '
-                    
-                    <li><a href="./installatie_zonphp.php">&raquo;&nbsp;' . $txt["insteltabel"] . '</a></li>
+                    <li><a href="installatie_zonphp.php">&raquo;&nbsp;' . $txt["insteltabel"] . '</a></li>
                     <li><a href="clear_tables.php">&raquo;&nbsp;' . $txt["deletevalues"] . '</a></li>
 					<li><a href="par_edit.php">&raquo;&nbsp;' . $txt["parameters"] . '</a></li>
 					<li><a href="par_referencevalues.php">&raquo;&nbsp;' . $txt["parref"] . '</a></li>
-					<li><a href="par_euro.php">&raquo;&nbsp;' . $txt["pareuro"] . '</a></li>
 					<li><a href="debug.php">&raquo;&nbsp;Debug</a></li> 
 					<li><a href="delete.php">&raquo;&nbsp;' . $txt["pardelete"] . '</a></li>
 					<li><a href="update.php">&raquo;&nbsp;' . $txt["parupdate"] . '</a></li>
@@ -26,17 +24,15 @@
                 }
                 ?>
             </ul>
-
             <?php
-            if (isset($con) && $con) {
-                echo " <hr>   <a href='?taal=nl' TARGET='_self'><img src='../inc/image/nl.svg' alt='nl' border='0' width='16'
-                                                       height='11'></a>
-                <a href='?taal=en' TARGET='_self'><img src='../inc/image/en.svg' alt='en' border='0' width='16'
-                                                       height='11'></a>
-                <a href='?taal=fr' TARGET='_self'><img src='../inc/image/fr.svg' alt='fr' border='0' width='16'
-                                                       height='11'></a>
-                <a href='?taal=de' TARGET='_self'><img src='../inc/image/de.svg' alt='de' border='0' width='16'
-                                                       height='11'></a> 
+            //if (isset($con) && $con) {
+                if (isset($_SESSION['passok']) && $_SESSION['passok'] == "passinorder") {
+                echo " 
+                <hr>   
+                <a href='?taal=nl' TARGET='_self'><img src='../inc/image/nl.svg' alt='nl' border='0' width='16' height='12'></a>
+                <a href='?taal=en' TARGET='_self'><img src='../inc/image/en.svg' alt='en' border='0' width='16' height='12'></a>
+                <a href='?taal=fr' TARGET='_self'><img src='../inc/image/fr.svg' alt='fr' border='0' width='16' height='12'></a>
+                <a href='?taal=de' TARGET='_self'><img src='../inc/image/de.svg' alt='de' border='0' width='16' height='12'></a> 
 			  ";
             }
             ?>
