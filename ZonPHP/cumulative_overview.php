6dd1b146b416f455922cd8e3ad99daf5df5fb84d
ericplan <48489161+ericplan@users.noreply.github.com>
2023-05-25 21:00:59 +0200

    Update cumulative_overview.php
    Only chart where the inverter choice isn't handled by Highcharts. Reintroduced butten and removed the reload which caused an infinite loop.

--- ZonPHP/cumulative_overview.php
+++ ZonPHP/cumulative_overview.php
@@ -5,12 +5,6 @@ include_once "inc/load_cache.php";
 include_once "inc/import_data.php";
 include_once "inc/header.php";
 include_once "charts/cumulative_chart.php";
-if (isset($_POST['add'])) {
-    echo '
-<script type="text/javascript">
-location.reload();
-</script>';
-}
 ?>
 <?php include "menu.php"; ?>
 <div id="page-content">
@@ -18,6 +12,7 @@ location.reload();
         <div id="week_chart_header" class="<?= HEADER_CLASS ?>">
             <h2 align="center">
                 <form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
+                   <button class="btn btn-primary" name='add' type="submit" value='+'>+</button>
                     <?php echo "&nbsp";
                     echo "  " . $title . " "; ?>
                 </form>
