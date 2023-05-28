d77cc3de1d8b5551d685db720be6378f7f0fa625
ericplan <48489161+ericplan@users.noreply.github.com>
2023-05-28 14:12:38 +0200

    Update import_data.php
    Removed mail functionality

--- ZonPHP/inc/import_data.php
+++ ZonPHP/inc/import_data.php
@@ -60,49 +60,4 @@ switch ($param['sInvullen_gegevens']) {
         echo 'Verkeerd';
         break;
 }
-
-
-if (!isset($param['next_mail_threshold'])) {
-    $sql = "SELECT Waarde FROM " . $table_prefix . "_parameters where variable = \"next_mail_threshold\" limit 1";
-    $result = mysqli_query($con, $sql) or die(header('location:opstart_installatie.php?fout=table'));
-    if (mysqli_num_rows($result) === 0) {
-        $sql = "INSERT INTO " . $table_prefix . "_parameters (variable, waarde) VALUES	('next_mail_threshold', 0)";
-        mysqli_query($con, $sql) or die ('next_mail_threshold:' . mysqli_error($con));
-        $param['next_mail_threshold'] = 0;
-    } else {
-        $row = mysqli_fetch_array($result);
-        $param['next_mail_threshold'] = $row['Waarde'];
-    }
-    $_SESSION['lastupdate'] = 0;
-};
-
-// sent mail if total kw is reached
-$totaal = round(0.5 + $total_sum_for_all_years / $param['mailinterval']) * $param['mailinterval'];
-if ($total_sum_for_all_years >= $param['next_mail_threshold']) {
-    //define the receiver of the email
-    $to = $param['email'];
-    //define the subject of the email
-    $subject = $param['sNaamVoorOpWebsite'] . $txt["mail1"] . number_format($total_sum_for_all_years, 0, ',', '.') . $txt["mail2"];
-    //define the message to be sent. Each line should be separated with \n
-    $message = $txt["mail3"] . "\n\n" . $param['sURL_link'] . "\n\n" . date("d/m H:i", time()) . " = " .
-        "kWh \n" . $txt["totaal"] . " = " . number_format($total_sum_for_all_years, 1, ',', '.') . "kWh \n";
-    //define the headers we want passed. Note that they are separated with \r\n
-    $headers = "From: " . $param['email'] . "\r\nReply-To: " . $param['email'];
-    //send the email
-    $mail_sent = @mail($to, $subject, $message, $headers);
-    //if the message is sent successfully print "Mail sent". Otherwise print "Mail failed"
-    // echo $mail_sent ? "Mail sent" : "Mail failed";
-
-    if ($mail_sent) {
-        error_log('---------> Mail sucessfully sent ' . $message);
-    } else {
-        error_log('######### Mail faild' . $message);
-    }
-
-    $param['next_mail_threshold'] = $totaal + $param['mailinterval'];
-    $sql = "update " . $table_prefix . "_parameters set waarde = " . $param['next_mail_threshold'] . " where variable = 'next_mail_threshold'";
-    mysqli_query($con, $sql) or die ('next_mail_threshold:' . mysqli_error($con));
-}
-
-
 ?>
