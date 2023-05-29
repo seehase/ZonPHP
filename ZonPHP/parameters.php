6f668ab2f59b5d285a96cafb8036b8be8bdea3ac
Holger Seehausen <seehausen@gmx.de>
2023-05-29 11:05:10 +0200

    changed paramerter a bit

--- ZonPHP/parameters.php
+++ ZonPHP/parameters.php
@@ -1,18 +1,12 @@
 <?php
-/*****************************************************************************
- * Set constants to get absolute paths when needed.
- * This is based on the relative position of this parameter.php file
- * ROOT_DIR gives the full absolute path to a file, including the install directory  
- * HTML_PATH gives the extension for use in href
- * PHP_PATH is HTML_PATH without the forward slash
- * ROOT_DIR = $_SERVER['DOCUMENT_ROOT'] + HTML_PATH
- *****************************************************************************/
-
-define('ROOT_DIR', realpath(__DIR__.'/'));
-define ('HTML_PATH',str_replace('\\', '/', substr(ROOT_DIR, strlen($_SERVER['DOCUMENT_ROOT']))));
-define ('PHP_PATH', ltrim( HTML_PATH, '/')); 
-// read version and debug configuration
-include_once "inc/version_info.php";
+/******************************************************************************
+ * Information
+ ******************************************************************************
+ * sample: https://solar.seehausen.org
+ *
+ * more infos under
+ * https://github.com/seehase/ZonPHP
+ ******************************************************************************/
 
 /*****************************************************************************
  * #### REQUIRED ####
@@ -31,23 +25,7 @@ $admin_password = " ";
  * #### REQUIRED ####
  * Database credentials and connection details  
  ******************************************************************************
- *
- * Hier kunt u server, username, password en databasenaam ingeven van uw database
- * $spassword wordt gebruikt voor inloggen in de database
- * 
- * Here you can enter server, username, password, database name for your database
- * $spassword is used for logging into the database
- * 
- * Ici, vous pouvez donner serveur, nom d'utilisateur, mot de passe, nom de la base 
- * pour l'entrée la base de données
- * $spassword est utilisé pour la connexion à la base de données *
- *
- * Hier können Sie Server, Benutzername, Passwort, Datenbank-Namen geben für Ihre Datenbank
- *
- * sample: https://solar.seehausen.org
- *
- * more infos under
- * https://github.com/seehase/ZonPHP
+ * Here you must enter server, username, password, database name for your database
  ******************************************************************************/
 
 $sserver = "localhost";                     /*  Database server   --> default: "localhost"               */
@@ -72,5 +50,20 @@ $weewx_database_name = "weewx";           /*  weewx Database name     --> defaul
 $weewx_table_name = "archive";            /*  weewx table name     --> default: "archive"              */
 $weewx_temp_column = "outTemp";           /*  weewx temp column name     --> default: "outTemp"        */
 $weewx_timestamp_column = "dateTime";     /*  weewx timestamp column name     --> default: "dateTime"  */
-$weewx_temp_is_farenheit = true           /*  weewx temp is in fahrenheit     --> default: "true"      */
+$weewx_temp_is_farenheit = true;          /*  weewx temp is in fahrenheit     --> default: "true"      */
+
+/*****************************************************************************
+ * Set constants to get absolute paths when needed.
+ * This is based on the relative position of this parameter.php file
+ * ROOT_DIR gives the full absolute path to a file, including the install directory
+ * HTML_PATH gives the extension for use in href
+ * PHP_PATH is HTML_PATH without the forward slash
+ * ROOT_DIR = $_SERVER['DOCUMENT_ROOT'] + HTML_PATH
+ *****************************************************************************/
+
+define('ROOT_DIR', realpath(__DIR__.'/'));
+define ('HTML_PATH',str_replace('\\', '/', substr(ROOT_DIR, strlen($_SERVER['DOCUMENT_ROOT']))));
+define ('PHP_PATH', ltrim( HTML_PATH, '/'));
+// read version and debug configuration
+include_once "inc/version_info.php";
 ?>
