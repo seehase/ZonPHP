a93b1ba082b70d2c17de187c9f87c16cb39a99fd
ericplan <48489161+ericplan@users.noreply.github.com>
2023-05-31 21:23:45 +0200

    Update year_overview.php
    Updated buttons, skipped unneeded code

--- ZonPHP/pages/year_overview.php
+++ ZonPHP/pages/year_overview.php
@@ -3,38 +3,10 @@ include_once "../parameters.php";
 include_once ROOT_DIR."/inc/sessionstart.php";
 include_once ROOT_DIR."/inc/load_cache.php";
 include_once ROOT_DIR."/inc/import_data.php";
-
 include_once ROOT_DIR."/inc/header.php";
 include_once "../charts/year_chart.php";
-
-$nextyearvisible = false;
-$nextyear = strtotime("+1 year", $chartdate);
-$nextyearstring = date("Y-01-01", strtotime("+1 year", $chartdate));
-if ($nextyear <= $date_maximum) {
-    $nextyearvisible = true;
-}
-$prevyear = strtotime("-1 year", $chartdate);
-$prevyearstring = date("Y-m-d", strtotime("-1 year", $chartdate));
-
-$date_minimum1 = strtotime("-1 year", $date_minimum);
-
-if ($prevyear >= $date_minimum1) {
-    $prevyearvisible = true;
-} else {
-    $prevyearvisible = false;
-}
-
-if ($nextyear <= $date_maximum) {
-    $nextyearvisible = true;
-}
-
-$chartyeardatestring = date("Y-01-01", strtotime("+0 year", $date_maximum));
-#$prevyear = date("Y-m-d", strtotime("-1 year", $chartdate));
-
 ?>
-
 <?php include ROOT_DIR."/inc/menu.php"; ?>
-
 <?php
 $paramstr_choose = '';
 $paramstr_day = '';
@@ -56,31 +28,22 @@ if (strpos($paramstr_choose, "?") == 0) {
     $paramstr_choose = '?' . $paramstr_choose;
 }
 ?>
-
 <div id="page-content">
-
-    <div id='resize' class="bigCharts" style="<?= WINDOW_STYLE_CHART ?>; padding-bottom: 59px; ">
+    <div id='resize' class="bigCharts" style="<?= WINDOW_STYLE_CHART ?>; padding-bottom: 56px; ">
         <div id="week_chart_header" class="<?= HEADER_CLASS ?>">
             <h2>
-                <?php
-                if ($prevyearvisible) {
-                    echo '<a class="btn btn-zonphp" href="year_overview.php' . $paramstr_day . 'jaar=' . $prevyearstring . '"> < </a>';
-                }
-                echo " " . $txt["jaar"] . " " . $datum . " ";
-                if ($nextyearvisible) {
-                    echo '<a class="btn btn-zonphp" href="year_overview.php' . $paramstr_day . 'jaar=' . $nextyearstring . '"> > </a>';
-                }
-                ?>
+                <button class="btn btn-zonphp" onclick="window.location.href='<?php echo '?jaar=' . date('Y-m', strtotime("-1 year", $chartdate)) . '\'"' ;
+				if (date('Y-m', $date_minimum) >= date('Y-m', $chartdate)) echo " hidden";	?>><i class="arrow left"></i></button>
+				<?php echo $datum ?>
+                <button class="btn btn-zonphp" onclick="window.location.href='<?php echo '?jaar=' . date('Y-m', strtotime("+1 year", $chartdate)) . '\'"' ;
+				if (date('Y-m', $date_maximum) <= date('Y-m', $chartdate)) echo " hidden";	?>><i class="arrow right"></i></button>
             </h2>
         </div>
         <div class="backtoday" style="float:none; position: absolute;  top: 15px;  left: 15px;">
-            <a href="<?php echo "year_overview.php" . $paramstr_day . "jaar=" . $chartyeardatestring ?>" target="_self">
-                <button class="btn btn-zonphp"><?php echo $txt["back_to_today"] ?></button>
-            </a>
+            <button class="btn btn-zonphp" onclick="window.location.href='<?php echo '?jaar='.date('Y', $chartcurrentdate); ?>'"><?php echo $txt["terugnaarvandaag"] ?></button>
         </div>
         <div id="year_chart" style=":width100%; height:100%;"></div>
     </div>
-
 </div>
 <script>
     $(document).ready(function () {
