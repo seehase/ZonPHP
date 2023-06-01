a05fd75b0afd32f4f8b527cafa8c0d0cf6d8f524
ericplan <48489161+ericplan@users.noreply.github.com>
2023-06-01 07:45:13 +0200

    Update all_years_overview.php
    HTML validator

--- ZonPHP/pages/all_years_overview.php
+++ ZonPHP/pages/all_years_overview.php
@@ -1,24 +1,16 @@
 <?php
-
 include_once "../parameters.php";
 include_once ROOT_DIR."/inc/sessionstart.php";
 include_once ROOT_DIR."/inc/load_cache.php";
 include_once ROOT_DIR."/inc/import_data.php";
-
 include_once ROOT_DIR."/inc/header.php";
-
 include_once "../charts/all_years_chart.php";
-
 // force reload caches
 if (isset($_SESSION['lastupdate'])) {
     $_SESSION['lastupdate'] = 0;
 }
-
 ?>
-
-
 <?php include ROOT_DIR."/inc/menu.php"; ?>
-
 <?php
 $paramstr_choose = '';
 $paramstr_day = '';
@@ -39,33 +31,22 @@ if (strpos($paramstr_day, "?") == 0) {
 if (strpos($paramstr_choose, "?") == 0) {
     $paramstr_choose = '?' . $paramstr_choose;
 }
-
 ?>
-
 <div id="page-content">
     <?php $myKeys = array_keys($sum_per_year); ?>
-
     <div id='resize' class="bigCharts" style="<?= WINDOW_STYLE_CHART ?>; padding-bottom: 41px;">
         <div id="week_chart_header" class="<?= HEADER_CLASS ?>">
             <h2><?php echo $txt["totaaloverzicht"] . "&nbsp;" . min($myKeys) . " - " . max($myKeys); ?></h2>
         </div>
-
         <div id="total_chart" style="width:100%; height:100%;"></div>
     </div>
-
-
 </div>
-
 <script>
     $(document).ready(function () {
         $("#resize ").height(500);
     });
 </script>
-
 </div><!-- closing ".page-content" -->
-
-</div><!-- closing ".container" -->
 <?php include_once ROOT_DIR."/inc/footer.php"; ?>
-
 </body>
 </html>
