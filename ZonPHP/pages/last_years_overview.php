881bd2e5f3bf50301b6e2b4042db02044a48225c
ericplan <48489161+ericplan@users.noreply.github.com>
2023-06-01 08:06:15 +0200

    Update last_years_overview.php
    HTML validator

--- ZonPHP/pages/last_years_overview.php
+++ ZonPHP/pages/last_years_overview.php
@@ -1,26 +1,18 @@
 <?php
-
 include_once "../parameters.php";
 include_once ROOT_DIR."/inc/sessionstart.php";
 include_once ROOT_DIR."/inc/load_cache.php";
 include_once ROOT_DIR."/inc/import_data.php";
-
 include_once ROOT_DIR."/inc/header.php";
 include_once "../charts/last_years_chart.php";
-
-
 $inverter = $_SESSION['Wie'];
 if (isset($_POST['inverter'])) {
     $inverter = $_POST['inverter'];
 }
-
 $nextyear = date("Y-m-d", strtotime("+1 year", $chartdate));
 $prevyear = date("Y-m-d", strtotime("-1 year", $chartdate));
-
 ?>
-
 <?php include_once ROOT_DIR."/inc/menu.php"; ?>
-
 <?php
 $paramstr_choose = '';
 $paramstr_day = '';
@@ -42,9 +34,7 @@ if (strpos($paramstr_choose, "?") == 0) {
     $paramstr_choose = '?' . $paramstr_choose;
 }
 ?>
-
 <div id="page-content">
-
     <div id='resize' class="bigCharts" style="<?= WINDOW_STYLE_CHART ?>; padding-bottom: 41px; ">
         <div id="chart_header" class="<?= HEADER_CLASS ?>">
             <h2>
@@ -55,23 +45,15 @@ if (strpos($paramstr_choose, "?") == 0) {
                 ?>
             </h2>
         </div>
-
         <div id="all_years_chart_<?php echo $inverter ?>" style="width:100%; height:100%; "></div>
     </div>
-
-
 </div>
-
 <script>
     $(document).ready(function () {
         $("#resize ").height(<?php echo $big_chart_height ?>);
     });
 </script>
-
 </div><!-- closing ".page-content" -->
-
-</div><!-- closing ".container" -->
 <?php include_once ROOT_DIR."/inc/footer.php"; ?>
-
 </body>
 </html>
