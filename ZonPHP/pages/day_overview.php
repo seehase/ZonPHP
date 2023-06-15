f5daf8786deb07db74949a565dbbbee476b6e009
ericplan <48489161+ericplan@users.noreply.github.com>
2023-06-15 13:27:49 +0200

    Update day_overview.php
    Replaced arrows because rendering in Safari failed at some display sizes
    Replaced jquery datepicker with bootstrap-datepicker including language support
    Minor style adjustments

--- ZonPHP/pages/day_overview.php
+++ ZonPHP/pages/day_overview.php
@@ -8,38 +8,23 @@ include_once "../charts/day_chart.php";
 ?>
 <?php include_once ROOT_DIR."/inc/menu.php"; ?>
 <script>
-	var start = '<?php echo date('Y-m-d', $date_minimum) ?>';
-    $(document).ready(function () {
-              
-        $("#startdate, #enddate").datepicker({
-            changeMonth: true,
-            changeYear: true,
-            showOn: "button",
-            gotoCurrent: false,
-            maxDate: 0,
-            
-            minDate: new Date(start),
-            buttonImage: "../inc/image/calendar.gif",
-            buttonImageOnly: true,
-            buttonText: "Select date",
-            onSelect: function () {
-                var url = "day_overview.php?dag=" + $("#startdate").val();
+	var start = '<?php echo date('Y-m-d',$date_minimum) ?>';
+	var language = '<?php echo substr($locale,0,2) ?>';
+    $(document).ready(function(){
+	$('#datepicker').datepicker( {
+    setDate: new Date(),
+    startDate: start,
+    endDate: '+0d',
+    language: language ,
+    todayHighlight: true,
+    autoclose: true,
+ 	});
+	$('#datepicker').datepicker().on('changeDate', function (e) {
+    var url = "day_overview.php?dag=" + e.format();
                 window.open(url, "_self");
-            },
-        });
-        $(".ui-datepicker-trigger").mouseover(function() {
-    		$(this).css('cursor', 'pointer');
-		});
-		$(".ui-datepicker-trigger").css("margin-bottom","-5px");
-        
-        var _gotoToday = jQuery.datepicker._gotoToday;
-        jQuery.datepicker._gotoToday = function (a) {
-            var target = jQuery(a);
-            var inst = this._getInst(target[0]);
-            _gotoToday.call(this, a);
-            jQuery.datepicker._selectDate(a, jQuery.datepicker._formatDate(inst, inst.selectedDay, inst.selectedMonth, inst.selectedYear));
-        };
-    });
+	//alert(language);
+	}); 
+	});
     </script>
 <?php
 $paramstr_choose = '';
@@ -61,24 +46,26 @@ if (strpos($paramstr_day, "?") == 0) {
 if (strpos($paramstr_choose, "?") == 0) {
     $paramstr_choose = '?' . $paramstr_choose;
 }
-
 ?>
 <div id="page-content">
     <div id='resize' class="bigCharts" style="<?= WINDOW_STYLE_CHART ?>; padding-bottom: 57px; ">
         <div id="week_chart_header" class="<?= HEADER_CLASS ?>">
-               <h2>
+                <h2>
                 <button class="btn btn-zonphp " onclick="window.location.href='<?php echo '?dag=' . date('Y-m-d', strtotime("-1 day", $chartdate)) . '\'"' ;
-				if ($date_minimum >= $chartdate) echo " hidden";	?>><i class="arrow left"></i></button>
+				if ($date_minimum >= $chartdate) echo " hidden";	?>><i class="fa fa-angle-left fa-lg"></i></button>
 				<?php echo $datum ?>
                 <button class="btn btn-zonphp" onclick="window.location.href='<?php echo '?dag=' . date('Y-m-d', strtotime("+1 day", $chartdate)) . '\'"' ;
-				if ($date_maximum <= $chartdate) echo " hidden";	?>><i class="arrow right text-align-top" ></i></button>
-               </h2> 
+				if ($date_maximum <= $chartdate) echo " hidden";	?>><i class="fa fa-angle-right fa-lg"></i></button>
+               </h2>
         </div>
   		<div class="backtoday align-top" style="none:left; position: absolute;  left: 15px;">
-            		<button class="btn btn-zonphp" onclick="window.location.href='<?php echo '?dag='.date('Y-m-d', $chartcurrentdate); ?>'"><?php echo getTxt("back_to_today") ?></button>
-		     			<div class="buttonbox" >
-        					<input type="hidden" id="startdate" readonly>
-        				</div>
+           		<button class="btn btn-zonphp" onclick="window.location.href='<?php echo '?dag='.date('Y-m-d', $chartcurrentdate); ?>'"><?php echo getTxt()"back_to_today") ?></button>
+		    <div class="buttonbox" >
+  				<div class="input-group date" id="datepicker" data-date-format="yyyy-mm-dd">	
+  					<input type='hidden' id='untilDate' class="form-control"   />
+    				<button class="btn btn-zonphp" ><i class="fa fa-calendar"></i></button>
+   				</div>
+	 		</div>
  		</div>
         <div id="mycontainer" style="width:100%; height:100%;"></div>
     </div>
