2f2d6605cbff0cb4fae852e6b56846d7239f9048
seehase <seehausen@gmx.de>
2023-06-27 09:52:34 +0200

    Fix TopMenu

--- ZonPHP/inc/topmenu.php
+++ ZonPHP/inc/topmenu.php
@@ -1,5 +1,5 @@
 <a href='<?= HTML_PATH ?>index.php' style="position:absolute; top:16px; left:80px;  border:0"><img
-            src="<?php echo HTML_PATH ?>inc/image/logo.svg" alt="ZonPHP logo" height="28" ></a>
+            src="<?php echo HTML_PATH ?>inc/image/logo.svg" alt="ZonPHP logo" height="28"></a>
 <?php if ($show_menu) echo '<label for="main-nav-check"  class="toggle" onclick="" title="Menu">&#x2261;</label>'; ?>
 <?php if (isActive('nl')) echo "<a href='?taal=nl' onclick=\"target='_self'\"><img src='" . HTML_PATH . "inc/image/blank.gif' 
  class='flag flag-nl' alt='Nederlands' title='Nederlands'></a>"; ?>
@@ -10,11 +10,6 @@
 <?php if (isActive('de')) echo "<a href='?taal=de' onclick=\"target='_self'\"><img src='" . HTML_PATH . "inc/image/blank.gif'
  class='flag flag-de' alt='deutsch' title='deutsch'></a>" ?>
 <?php
-if (count(PLANTS) == 1) {
-    {
-        echo '<p id="headerinverter" style="vertical-align: middle; position:absolute; top:16px; left:360px;">' .
-            $params['plant']['name'] .
-            '</p>';
-    }
-}
+echo '<p id="headerinverter" style="vertical-align: middle; position:absolute; top:16px; left:360px;">' .
+    $params['plant']['name'] . '</p>';
 ?>
