bbd9b219c8807aedb3b4ec81ebfb525267b43763
seehase <seehausen@gmx.de>
2023-10-14 16:45:10 +0200

    Update version

--- ZonPHP/inc/version_info.php
+++ ZonPHP/inc/version_info.php
@@ -3,5 +3,5 @@
  * Version
  *********************************************************************/
 $zonPHPSessionID = "SOLAR14";    // Change SessionId if needed e.g. if you run multi instances
-$version = "v2023.08.27 (dev)";
+$version = "v2023.10.07 (dev)";
 
