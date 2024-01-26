c697741d55385d5da62a5e9c7c8c9cc6fe8fa5ca
ericplan <48489161+ericplan@users.noreply.github.com>
2024-01-26 11:54:37 +0100

    Update version_info.php
    default update

--- ZonPHP/inc/version_info.php
+++ ZonPHP/inc/version_info.php
@@ -2,7 +2,7 @@
 /********************************************************************
  * Version
  *********************************************************************/
-$version = "v3.2.8";
+$version = "v3.2.9";
 
 // Change SessionId if needed e.g. if you run multi instances
 $zonPHPSessionID = "SOLAR_" . str_replace('.', '_', $version);
