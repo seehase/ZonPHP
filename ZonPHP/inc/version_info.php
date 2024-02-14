a537a577226fd99bfb2b4d2d221777fe9d9b4ee0
seehase <seehausen@gmx.de>
2024-02-14 10:35:35 +0100

    Introduced new filter options for TOP chart on months and years

--- ZonPHP/inc/version_info.php
+++ ZonPHP/inc/version_info.php
@@ -2,7 +2,7 @@
 /********************************************************************
  * Version
  *********************************************************************/
-$version = "v3.3.5";
+$version = "v3.3.6";
 
 // Change SessionId if needed e.g. if you run multi instances
 $zonPHPSessionID = "SOLAR_" . str_replace('.', '_', $version);
