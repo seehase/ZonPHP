a124b2dd6f749ee0801d8ba83ea9e76dc8befa80
seehase <seehausen@gmx.de>
2025-01-03 17:31:48 +0100

    Fix day chart if data is not rounded to minutes

--- ZonPHP/inc/footer.php
+++ ZonPHP/inc/footer.php
@@ -3,7 +3,7 @@ global $params, $colors, $version, $new_version_label;
 ?>
 
 <div id="footer" style="background-color: <?= $colors['color_footerbackground'] ?>; display: clear:both; ">
-    <?= "ZonPHP " . $version . "&#946;" ?>
+    <?= "ZonPHP " . $version ?>
     <span class="smallFooter">
         &nbsp;-&nbsp;<b>PHP8 compliant</b>&nbsp;-&nbsp;
         <a href="https://github.com/seehase/ZonPHP" onclick="target='_blank'">https://github.com/seehase/ZonPHP</a>
