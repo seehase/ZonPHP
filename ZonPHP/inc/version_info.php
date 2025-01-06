22add01bd401e3e8c2cd1c59517600585ee9cfd1
seehase <seehausen@gmx.de>
2025-01-06 18:06:16 +0100

    Bumb version to 4.1.0

--- ZonPHP/inc/version_info.php
+++ ZonPHP/inc/version_info.php
@@ -3,7 +3,7 @@
  * Version
  *********************************************************************/
 
-$version = "v4.0.18";
+$version = "v4.1.0";
 
 // Change SessionId if needed e.g. if you run multi instances
 $zonPHPSessionID = "SOLAR_" . str_replace('.', '_', $version);
