1c773acacf0f628d62111f88eaa9fdc6a222f5fc
Holger Seehausen <seehausen@mediamarktsaturn.com>
2026-04-04 12:19:00 +0200

    fix: DayChart use string instead of int, because it can fail on 32-bit systems

--- ZonPHP/inc/version_info.php
+++ ZonPHP/inc/version_info.php
@@ -3,7 +3,7 @@
  * Version
  *********************************************************************/
 
-$version = "v4.4.5";
+$version = "v4.4.6";
 
 // Change SessionId if needed e.g. if you run multi instances
 $zonPHPSessionID = "SOLAR_" . str_replace('.', '_', $version);
